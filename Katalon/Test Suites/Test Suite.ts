1082a7c79462f478f299a01d2d6689231b8fe4a2
hilmiiaziiz <53106495+hilmiiaziiz@users.noreply.github.com>
2021-10-31 18:57:49 +0700

    Add files via upload

---
+++ Katalon/Test Suites/Test Suite.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>b425fd60-c531-4474-9cba-6bed84fde78a</testSuiteGuid>
+   <testCaseLink>
+      <guid>edeb450d-ad64-48a0-ba97-3348f56c0544</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/RunCucumberFeature</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
